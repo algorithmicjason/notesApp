a320e587d6f77639e5e36303794e9e898dcead5a
Jason Herrera <baytownjason@yahoo.com>
2021-09-29 23:54:59 -0500

    updated Header appearance

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,19 +11,11 @@ const Header = () => {
       }}
       elevation={3}
     >
-      <img
-        src="https://mpng.subpng.com/20180604/pol/kisspng-react-javascript-angularjs-ionic-atom-5b154be6709500.6532453515281223424611.jpg"
-        alt=""
-        style={{ width: "90px" }}
-      />
-      <h1 style={{ margin: "0", padding: "10px", fontFamily: "cursive" }}>
+      
+      <h1 style={{ fontSize: "50px", margin: "0", padding: "10px", fontFamily: "cursive" }}>
         React Notes
       </h1>
-      <img
-        src="https://mpng.subpng.com/20180604/pol/kisspng-react-javascript-angularjs-ionic-atom-5b154be6709500.6532453515281223424611.jpg"
-        alt=""
-        style={{ width: "90px" }}
-      />
+      
     </Paper>
   );
 };
